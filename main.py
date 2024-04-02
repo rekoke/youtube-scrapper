774883db1936cac02489f89e88267cb697fadc1b
koke <rekoke92@gmail.com>
2024-04-02 15:48:42 +0200

    Tyding code and making dewscription optional in pydantic

--- main.py
+++ main.py
@@ -4,43 +4,51 @@ import requests
 import json
 import logging
 
-from youtube_api import YoutubeScrappedVideo, YoutubeScrapeResult
+from youtube_scrapper import YoutubeScrappedVideo, YoutubeScrapeResult
 
 logger = logging.getLogger(__name__)
 
-video_id = "burton"
-video_url = "https://www.youtube.com/results?search_query=" + video_id
-response = requests.request("GET", video_url)
-soup = BeautifulSoup(response.text, "html.parser")
-body = soup.find_all("body")[0]
-scripts = body.find_all("script")
 
-result = json.loads(scripts[13].string[20:-1])
+class YoutubeScraping():
 
-estimatedResults = result['estimatedResults']
+    def findkeys(self, node, kv):
+        if isinstance(node, list):
+            for i in node:
+                for x in self.findkeys(i, kv):
+                    yield x
+        elif isinstance(node, dict):
+            if kv in node:
+                yield node[kv]
+            for j in node.values():
+                for x in self.findkeys(j, kv):
+                    yield x
+
+    def getYoutubeScrapeResults(self, search_term: str):
+        youtube_url = "https://www.youtube.com/results?search_query="
+        video_url = youtube_url + search_term
+
+        response = requests.request("GET", video_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        body = soup.find_all("body")[0]
+        scripts = body.find_all("script")
+        data = json.loads(scripts[13].string[20:-1])
+
+        scraped_results: list[YoutubeScrappedVideo] = []
+        video_renderer_list = list(self.findkeys(data, 'videoRenderer'))
+
+        try:
+            for video in video_renderer_list:
+                myvid = video
+                print(myvid)
+                scraped_results.append(YoutubeScrapeResult(**video).to_youtube_scrapping_result())
+            return scraped_results
+
+        except ValidationError as e:
+            logger.error(f"Unable to parse Pydantic models from Youtube response.\n\n{e}")
+
+    def run_interaction(self, search_term: str):
+        return self.getYoutubeScrapeResults(search_term)
 
 
-def findkeys(node, kv):
-    if isinstance(node, list):
-        for i in node:
-            for x in findkeys(i, kv):
-                yield x
-    elif isinstance(node, dict):
-        if kv in node:
-            yield node[kv]
-        for j in node.values():
-            for x in findkeys(j, kv):
-                yield x
-
-
-video_renderer_list = list(findkeys(result, 'videoRenderer'))
-scrape_results: list[YoutubeScrappedVideo] = []
-
-
-try:
-    for video in video_renderer_list:
-        scrape_results.append(YoutubeScrapeResult(**video).to_youtube_scrapping_result())
-except ValidationError as e:
-    logger.error(f"Unable to parse Pydantic models from Youtube response.\n\n{e}")
-
-print(scrape_results)
+sc = YoutubeScraping().run_interaction('trump')
+print(sc)
---
+++ youtube_scrapper.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class YoutubeScrappedVideo(BaseModel):
+    id: str
+    title: str
+    published_time: str
+    view_count: str
+    channel_name: str
+    description: str | None
+
+
+class YoutubeRunsText(BaseModel):
+    text: str | None
+
+
+class YoutubeRuns(BaseModel):
+    runs: list[YoutubeRunsText] | None
+
+
+class YoutubePublishedTimeText(BaseModel):
+    simpleText: str
+
+
+class YoutubeViewCountText(BaseModel):
+    simpleText: str
+
+
+class YoutubedetailedMetadataSnippetItem(BaseModel):
+    snippetText: YoutubeRuns
+
+
+class YoutubeScrapeResult(BaseModel):
+    videoId: str
+    title: YoutubeRuns
+    publishedTimeText: YoutubePublishedTimeText
+    viewCountText: YoutubeViewCountText
+    ownerText: YoutubeRuns
+    detailedMetadataSnippets: Optional[list[YoutubedetailedMetadataSnippetItem]] = None
+
+    def to_youtube_scrapping_result(self) -> YoutubeScrappedVideo:
+        return YoutubeScrappedVideo(
+            id=self.videoId,
+            title=self.title.runs[0].text,
+            published_time=self.publishedTimeText.simpleText,
+            view_count=self.viewCountText.simpleText,
+            channel_name=self.ownerText.runs[0].text,
+            description=self.detailedMetadataSnippets[0].snippetText.runs[0].text if self.detailedMetadataSnippets else None,
+        )
